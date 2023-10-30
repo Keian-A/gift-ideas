f32a81defdbf21489fd5f7f0fa1a32cf8f7106ac
Keian Anthony <KeianAnthony1@gmail.com>
2023-10-29 18:53:44 -0700

    changes made in airport to navigate you back to main page when you click on website logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Header from './components/Header/Header.jsx';
 import Login from './components/Login/Login.jsx';
 import Home from './components/Home/Home.jsx';
+// import { Route } from 'react-router-dom';
 
 function App() {
 
@@ -12,7 +13,8 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
+      <Header setValidated={setValidated} setList={setList} setCurrentMember={setCurrentMember} />
+      {/* <Route path='/' element={<Login />} /> */}
       {validated ?
         null
         :
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
 import './Header.css';
+// import { useNavigate } from 'react-router-dom';
 
-function Header() {
+function Header({ setValidated, setList, setCurrentMember }) {
+    // const navigate = useNavigate();
+
+    const mainNav = () => {
+        // navigate('/');
+        setCurrentMember('');
+        setList("");
+        setValidated(false);
+    }
+
     return (
         <div id="header">
-            <h1>Gift Ideas</h1>
+            <button id="header-button" onClick={() => mainNav()}>Gift Ideas</button>
         </div>
     );
 }
