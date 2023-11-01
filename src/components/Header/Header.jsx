79304c33542636455cb81ca7d06c719c9c967b1a
Keian Anthony <KeianAnthony1@gmail.com>
2023-11-01 15:09:48 -0700

    adds button and route to server to delete all items marked as bought on personal list

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
+import axios from 'axios';
 import { Stack, Button } from '@mui/material';
 import './Header.css';
 
-function Header({ setValidated, setList, setCurrentMember, list }) {
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
+function Header({ currentMember, setValidated, setList, setCurrentMember, list }) {
 
     const mainNav = () => {
         setCurrentMember('');
@@ -9,12 +12,23 @@ function Header({ setValidated, setList, setCurrentMember, list }) {
         setValidated(false);
     }
 
+    const removePersonalBought = async () => {
+        try {
+            let giftObj = {username: currentMember};
+            let {data} = await axios.post(`${SERVER_URL}/deleteMyBoughtGifts`, giftObj);
+            setList(data);
+        } catch (e) {
+            console.error(e.message);
+        }
+    }
+
     return (
         <div id="header">
             <h2 id="title">Gift Ideas</h2>
             <div id="nav-bar">
                 {list ? (
-                    <Stack>
+                    <Stack id="nav-links">
+                        <Button variant="outlined" onClick={() => removePersonalBought()}>Remove All My Bought Items</Button>
                         <Button variant="outlined" onClick={() => mainNav()}>Sign Out</Button>
                     </Stack>
                 ) : null}
